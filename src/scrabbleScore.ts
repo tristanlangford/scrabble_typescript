b2f82b3f30e4f09458a4a9aca281cdb04fc83a68
Tristan Langford <tristanlangford@hotmail.com>
2020-08-24 18:31:55 +0100

    a returns 1

---
+++ src/scrabbleScore.ts
@@ -0,0 +1,5 @@
+function scrabbleScore(word: string) {
+    return 1
+}
+
+module.exports = scrabbleScore
--- test/scrabble.test.js
+++ test/scrabble.test.js
@@ -3,7 +3,7 @@ const scrabbleScore = require('../src/scrabbleScore')
 describe('scrabbleScore', function() {
 
     test('a returns 1', function() {
-        expect(scrabbleScore('a')).toEq(1)
+        expect(scrabbleScore('a')).toEqual(1)
     })
 
 })
